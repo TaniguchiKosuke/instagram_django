cec48151946a8f52a9d0527d33a7043bf38ad565
ti-yapitt <applekkttegg@yahoo.co.jp>
2021-09-20 16:46:57 +0900

    fix

--- instagram/views.py
+++ instagram/views.py
@@ -90,6 +90,7 @@ class HomeView(LoginRequiredMixin, ListView):
         if query:
             context['query_exist'] = True
             if query.startswith('#'):
+                context['query'] = query
                 context['tags'] = PostTagRelation.objects.filter(tag__name__icontains=query)
             else:
                 context['query'] = query
@@ -239,9 +240,8 @@ class UserProfileView(LoginRequiredMixin, ListView):
     paginate_by = 15
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         user = User.objects.get(pk=self.kwargs['pk'])
-        queryset = queryset.objects.filter(author=user).order_by('-created_at')
+        queryset = Posts.objects.filter(author=user).order_by('-created_at')
         return queryset
 
     def get_context_data(self, **kwargs):
@@ -416,7 +416,6 @@ class FolloweeListView(LoginRequiredMixin, ListView):
     paginate_by = 16
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         user = User.objects.get(pk=self.kwargs['pk'])
         queryset = User.objects.filter(followers=user)
         return queryset
@@ -437,7 +436,6 @@ class FollowerListView(LoginRequiredMixin, ListView):
     paginate_by = 16
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         user = User.objects.get(pk=self.kwargs['pk'])
         queryset = User.objects.filter(followees=user)
         return queryset
@@ -458,7 +456,6 @@ class MessagesView(LoginRequiredMixin, ListView):
     paginate_by=20
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         request_user = self.request.user
         queryset = Message.objects.filter(Q(to_user=self.kwargs['pk']) | Q(from_user=self.kwargs['pk']))\
                     .filter(Q(from_user=request_user.pk) | Q(to_user=request_user)).order_by('created_at')
@@ -494,7 +491,6 @@ class MessageListView(LoginRequiredMixin, ListView):
     queryset = User
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         request_user = self.request.user
         queryset = User.objects.filter(followers=request_user)[:10]
         #もしDMが来てるもしくは、リクエストユーザーが誰かにDMを送っている場合はその人を優先的にリストアップする処理
@@ -550,7 +546,6 @@ class TagPostListView(LoginRequiredMixin, ListView):
     paginate_by = 27
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         tag = self.kwargs['tag']
         queryset = PostTagRelation.objects.filter(tag__name=tag)
         return queryset
@@ -561,6 +556,10 @@ class TagPostListView(LoginRequiredMixin, ListView):
         context['tag'] = tag
         context['num_of_posts'] = PostTagRelation.objects.filter(tag__name=tag).count()
         context['request_user'] = self.request.user
+        tag = self.kwargs['tag']
+        post_tag_relation_first = PostTagRelation.objects.filter(tag__name=tag)
+        if post_tag_relation_first:
+            context['first_post'] = post_tag_relation_first.first().post
         return context
 
 
@@ -570,7 +569,6 @@ class SearchFriendsView(LoginRequiredMixin, ListView):
     paginate_by = 16
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         user = self.request.user
         search_friends = self.request.GET.get('search_friends')
         if search_friends:
@@ -632,7 +630,6 @@ class ReccomendedPostsView(LoginRequiredMixin, ListView):
         ログインユーザーへのおすすめの投稿を
         リストアップするメソッド
         """
-        queryset = super().get_queryset()
         request_user = self.request.user
         #ログインユーザーがいいねした投稿のタグに基づいて、おすすめの投稿を取得
         liked_posts = PostLikes.objects.filter(user=request_user)
@@ -683,7 +680,6 @@ class SeeAllReccomendedUsersView(LoginRequiredMixin, ListView):
     paginate_by = 16
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         request_user = self.request.user
         followee_friendships = FriendShip.objects.filter(follower__username=request_user)
         follower_friendships = FriendShip.objects.filter(followee__username=request_user)
@@ -707,7 +703,6 @@ class LikedPostListView(LoginRequiredMixin, ListView):
     paginate_by = 27
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         request_user = self.request.user
         post_likes = PostLikes.objects.filter(user=request_user).order_by('-created_at')
         liked_post_list = []
@@ -729,7 +724,6 @@ class LikedPostUserView(LoginRequiredMixin, ListView):
     paginate_by = 16
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         post = Posts.objects.get(pk=self.kwargs['pk'])
         post_likes = PostLikes.objects.filter(post=post)
         queryset = []
@@ -754,7 +748,6 @@ class SavedPostListView(LoginRequiredMixin, ListView):
     paginate_by = 15
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         user = User.objects.get(pk=self.kwargs['pk'])
         post_save = PostSave.objects.filter(user=user)
         saved_post_list = []
